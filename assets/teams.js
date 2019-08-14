9665f4cfe70149ccd699ce8223e9516d58973a5b
mattwills09 <mattwills09@gmail.com>
2019-08-14 10:59:25 -0400

    updated JS

---
+++ assets/teams.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+// executes when HTML-Document is loaded and DOM is ready
+   console.log("document is ready");
+   
+    $(".card").hover(
+        function() {
+            $(this).addClass('shadow-lg text-danger').css('cursor', 'pointer'); 
+        }, function() {
+            $(this).removeClass('shadow-lg text-danger');
+        }
+    );
+
+    $(".card-title").hover(
+        function() {
+            $(this).addClass('text-primary').css('cursor', 'pointer'); 
+        }, function() {
+            $(this).removeClass('text-primary');
+        }
+    );
+     
+   // document ready  
+   });
